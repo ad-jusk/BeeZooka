f427995f9b4f605f13aa51e953d6b5a3224ce999
Adam <adamj1215@gmail.com>
2024-05-03 14:39:26 +0200

    added StartScene, switching between scenes and TextMesh Pro package for UI

---
+++ Assets/Scripts/SceneManagerScript.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour
+{
+    public void LoadScene(string sceneName) {
+        SceneManager.LoadScene(sceneName);
+    }
+}
