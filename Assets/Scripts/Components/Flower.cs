322a209073e55a254bb2cc413335fb2be068579f
Adam <adamj1215@gmail.com>
2024-05-17 00:07:37 +0200

    logic for flowers and beehive

--- Assets/Scripts/Components/Flower.cs
+++ Assets/Scripts/Components/Flower.cs
@@ -2,5 +2,41 @@ using UnityEngine;
 
 public class Flower : MonoBehaviour
 {
+    private enum FlowerState {
+        ACTIVE, INACTIVE
+    };
 
+    [SerializeField]
+    private FlowerColor flowerColor = FlowerColor.RED; // RED BY DEFAULT
+    private FlowerState flowerState = FlowerState.ACTIVE;
+
+    private GameManager gameManager;
+
+    private void Awake() {
+        gameManager = GameManager.Instance;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+
+        if(flowerState == FlowerState.INACTIVE) {
+            Debug.Log("Inactive flower hit - ignoring collision");
+            return;
+        }
+
+        gameManager.NotifyFlowerEntered(flowerColor);
+
+        Rigidbody2D beeRigidbody = collision.transform.GetComponent<Rigidbody2D>();
+        beeRigidbody.velocity = Vector2.zero;
+        beeRigidbody.position = transform.GetComponent<Renderer>().bounds.center;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision) {
+        SetFlowerInactive();
+    }
+
+    private void SetFlowerInactive() {
+        flowerState = FlowerState.INACTIVE;
+        // ALSO DISABLE COLLIDER
+        GetComponent<Collider2D>().enabled = false;
+    }
 }
--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 [DefaultExecutionOrder(-1)]
@@ -10,7 +12,23 @@ public class GameManager : Singleton<GameManager>
     public delegate void BeehiveMissed();
     public event BeehiveMissed OnBeehiveMissed;
 
+    public delegate void FlowerEntered(FlowerColor flowerColor);
+    public event FlowerEntered OnFlowerEntered;
+
     #endregion
+
+
+    #region  GameStats
+
+    public List<FlowerColor> flowersToCollect;
+    public List<FlowerColor> collectedFlowers;
+
+    #endregion
+
+    private void Awake() {
+        flowersToCollect = new() { FlowerColor.RED, FlowerColor.PURPLE };
+        collectedFlowers = new();
+    }
 
     public void NotifyBeehiveEntered() {
         OnBeehiveEntered?.Invoke();
@@ -18,5 +36,37 @@ public class GameManager : Singleton<GameManager>
 
     public void NotifyBeehiveMissed() {
         OnBeehiveMissed?.Invoke();
+    }
+
+    public void NotifyFlowerEntered(FlowerColor flowerColor) {
+        collectedFlowers.Add(flowerColor);
+        OnFlowerEntered?.Invoke(flowerColor);
+    }
+
+    public bool AllFlowersCollected() {
+
+        if (flowersToCollect.Count != collectedFlowers.Count){
+            return false;
+        }
+
+        var itemCounts = new Dictionary<FlowerColor, int>();
+
+        foreach (FlowerColor s in flowersToCollect) {
+            if (itemCounts.ContainsKey(s)) {
+                itemCounts[s]++;
+            } else {
+                itemCounts.Add(s, 1);
+            }
+        }
+
+        foreach (FlowerColor s in collectedFlowers) {
+            if (itemCounts.ContainsKey(s)) {
+                itemCounts[s]--;
+            }
+            else {
+                return false;
+            }
+        }
+        return itemCounts.Values.All(c => c == 0);
     }
 }
