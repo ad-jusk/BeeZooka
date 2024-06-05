37bd0c12ec445a1cc77c2a998eab75b475630fc6
Adam <adamj1215@gmail.com>
2024-06-05 23:02:29 +0200

    added serializable dictionary

--- Assets/Scripts/DataPersistance/Data/GameData.cs
+++ Assets/Scripts/DataPersistance/Data/GameData.cs
@@ -4,12 +4,14 @@ public class GameData
     public bool musicEnabled;
     public bool sfxEnabled;
     public int levelsCleared;
+    public SerializableDictionary<int, int> levelIndexToCollectables;
 
     public GameData()
     {
         this.musicEnabled = true;
         this.sfxEnabled = true;
         this.levelsCleared = 0;
+        this.levelIndexToCollectables = new SerializableDictionary<int, int>();
     }
 
     public override string ToString()
